482031efbaf21478872a8fb5411f02132e588f3e
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-10-16 17:43:47 +0500

    Users New Demo

--- src/Components/newuser/NewUserForm.jsx
+++ src/Components/newuser/NewUserForm.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 function NewUserForm({addUser}) {
   const [user, setUser] = useState({
     id: uuidv4(),
-    image: "",
+    image: "https://picsum.photos/id/870/200/300?grayscale&blur=2",
     firstName: "",
     lastName: "",
     age: null,
